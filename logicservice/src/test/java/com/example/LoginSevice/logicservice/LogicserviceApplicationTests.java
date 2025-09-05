ca4b84a94e281f5454b6f5be2ada282212678732
Priydev Mishra <priydevmishra389@gmail.com>
2025-09-05 19:32:22 +0530

    added new folder for backend

---
+++ logicservice/src/test/java/com/example/LoginSevice/logicservice/LogicserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginSevice.logicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
